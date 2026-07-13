8cc876cbc298a5e16d22f715a45a8ed7b63ba0e1
min <wittgen0024@gmail.com>
2026-07-14 00:48:55 +0900

    feat: [김정민] 아키텍처 포스트 추가

--- src/blog/min/index.ts
+++ src/blog/min/index.ts
@@ -1,7 +1,8 @@
 import type { BlogPost } from '../../data';
 import { post01 } from './01-term-entity';
 import { post02 } from './02-tsv-spring-batch';
+import { post03 } from './03-architecture';
 
 export const minPosts: BlogPost[] = [
-    post01, post02
+    post01, post02, post03
 ];
