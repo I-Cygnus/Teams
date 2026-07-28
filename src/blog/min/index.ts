b2852bf85041c87d725b1c9a9658c5c2a93b3df9
min <wittgen0024@gmail.com>
2026-07-28 21:40:09 +0900

    docs: [김정민] RAG 문서 검색 구조 정리

--- src/blog/min/index.ts
+++ src/blog/min/index.ts
@@ -2,7 +2,8 @@ import type { BlogPost } from '../../data';
 import { post01 } from './01-term-entity';
 import { post02 } from './02-tsv-spring-batch';
 import { post03 } from './03-architecture';
+import { post04 } from './04-rag-architecture';
 
 export const minPosts: BlogPost[] = [
-    post01, post02, post03
+    post01, post02, post03, post04
 ];
