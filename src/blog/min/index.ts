3a9aabd79d4b30661b0630b5d616c903b1e34c61
min <wittgen0024@gmail.com>
2026-07-31 01:26:05 +0900

    docs: [김정민] 키워드 검색과 의미 기반 검색 비교

--- src/blog/min/index.ts
+++ src/blog/min/index.ts
@@ -3,7 +3,9 @@ import { post01 } from './01-term-entity';
 import { post02 } from './02-tsv-spring-batch';
 import { post03 } from './03-architecture';
 import { post04 } from './04-rag-architecture';
+import { post05 } from './05-keyword-semantic-search';
+import { post06 } from './06-embedding-vector-search';
 
 export const minPosts: BlogPost[] = [
-    post01, post02, post03, post04
+    post01, post02, post03, post04, post05, post06
 ];
