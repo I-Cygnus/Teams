f4082288e1968d44d624859f294fe7e14763da71
min <wittgen0024@gmail.com>
2026-08-22 00:55:25 +0900

    docs: [김정민] AWS와 자체 서버 비교 기술

--- src/blog/min/index.ts
+++ src/blog/min/index.ts
@@ -5,7 +5,10 @@ import { post03 } from './03-architecture';
 import { post04 } from './04-rag-architecture';
 import { post05 } from './05-keyword-semantic-search';
 import { post06 } from './06-embedding-vector-search';
+import { post07 } from './07-aws-vs-on-premise';
+import { post08 } from './08-on-premise-server-operation';
+import { post09 } from './09-web-accessibility';
 
 export const minPosts: BlogPost[] = [
-    post01, post02, post03, post04, post05, post06
+    post01, post02, post03, post04, post05, post06, post07, post08, post09
 ];
